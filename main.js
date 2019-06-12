8c1dc73d3a00ceaf9c22d7d168992604c617e1d8
Thionazin <49171006+Thionazin@users.noreply.github.com>
2019-06-11 21:50:07 -0500

    added social credit system

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
+//setup for mongoose
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/netizendb', {useNewUrlParser: true});
+const Netizen = require("./citizens.js");
 
 var bannedWords = ['天安门', 'tiananmen', '六四', '6/4', "动态网自由门", "天安門", "天安门", "法輪功", "李洪志", "六四天安門事件", "天安門大屠殺", "反右派鬥爭", "大躍進政策]", "文化大革命", "人權", "民運", "自由", "獨立", "多黨制", "台灣", "臺灣", "中華民國", "西藏", "土伯特", "唐古特", "達賴喇嘛", "法輪功", "新疆維吾爾自治區", "諾貝爾和平獎", "劉暁波", "民主", "言論", "思想", "反共", "反革命", "抗議", "運動", "騷亂", "暴亂", "騷擾", "擾亂", "抗暴", "平反", "維權", "示威游行", "李洪志", "法輪大法", "大法弟子", "強制斷種", "強制堕胎", "民族淨化", "人體實驗", "肅清", "胡耀邦", "趙紫陽", "魏京生", "王丹", "還政於民", "和平演變", "激流中國", "北京之春", "大紀元時報", "九評論共産黨", "獨裁", "專制", "壓制", "統一", "監視", "鎮壓", "迫害", "侵略", "掠奪", "破壞", "拷問", "屠殺", "活摘器官", "誘拐", "買賣人口", "遊進", "走私", "毒品", "賣淫", "春畫", "賭博", "六合彩", "天安門", "天安门", "法輪功", "李洪志", "劉曉波动态网自由门"]
 var curseWords = ['fuck', 'fuck you', 'cao', '草','曹']
@@ -67,13 +71,91 @@ function processCommand(message) {
                 }
             )
             break
+
+        case "score":
+          printScore(message)
+          break
+
+
     }
 
 }
 
+//modifies the social credit score
+function modifyScore(message, amount)
+{
+  //checks to see if user is a party member
+  //if(msg.member.roles.has(msg.guild.roles.find("name", "Party Official").id))
+  //{
+  //  return;
+  //}
+  //else{
+  //finds the entry for the netizen
+  Netizen.findOne({
+    userID: message.author.id
+  }, (err, creditScore) => {
+    if(err) console.log(err);
+    if(!creditScore){
+      const newScore = new Netizen({
+        userID: message.author.id,
+        creditScore: 1000
+      })
+      newScore.save().catch(err => console.log(err));
+    }
+    else
+    {
+      creditScore.creditScore = creditScore.creditScore + amount;
+      creditScore.save().catch(err => console.log(err));
+    }
+  })
+  }
+//}
+
+
+//gets the social credit score
+function printScore(message)
+{
+  const args = message.content.slice(1).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+  if(args.length != 1)
+  {
+    return;
+  }
+
+
+  if(message.member.roles.has(message.guild.roles.find("name", "Party Official").id))
+  {
+    Netizen.findOne({
+      userID: message.mentions.users.first().id
+    }, (err, creditScore) => {
+      if(err) console.log(err);
+      if(!creditScore){
+
+        message.channel.send("这个人不存在。")
+
+      }
+      else
+      {
+        message.channel.send(creditScore.creditScore)
+      }
+    })
+  }
+  else
+  {
+    return;
+  }
+
+}
+
+
+
+
 // Message interceptor
 client.on('message', (message) => {
-    if(message.author.id == 584213592602181632 || message.channel.id == 587302958085963787) return
+    //bad code that you should change
+    //only allows for one specific instance
+    //better to match with channel names
+    if(message.author.id == 584213592602181632 || message.channel.id == 587302958085963787 || message.author.bot) return
     // console.log(message)
     let m = message.content
     m = m.toLowerCase() === undefined ? m : m.toLowerCase()
@@ -84,13 +166,17 @@ client.on('message', (message) => {
     if(phraseMatchesList(m, bannedWords)) {
         message.delete()
         message.channel.send("警告：请"+message.author+"同志不要没有依据地制造污蔑我国政府的假新闻！")
+        modifyScore(message, -50)
     } else if(phraseMatchesList(m, curseWords)) {
         message.channel.send("不，你。")
         message.channel.send({files: ["./cnm.png"]})
+        modifyScore(message, -10)
     } else if (phraseMatchesList(m, fakeThings)) {
         message.channel.send("Doesn't exist and is a lie.")
+        modifyScore(message, -20)
     } else if (phraseMatchesList(m, offensive)) {
         message.channel.send("Hey you! "+message.author+" \nSTFU!")
+        modifyScore(message, -10)
     } else if (m.match("ping")) {
         message.channel.send("Ping你妈呀！")
     }
